138a01108f157eaaaa045070fa50a9dda884f462
Nandana Boban <nandanaboban03@gmail.com>
2024-07-26 20:30:25 +0530

    created

---
+++ PortfolioProject/Controllers/EducationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class EducationController : Controller
+    {
+        public IActionResult Education()
+        {
+            return View();
+        }
+    }
+}
